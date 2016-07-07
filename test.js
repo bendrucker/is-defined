49e1b3facf2e1381c2856f56ebba4b731ea908c6
Lewie <lewismoten@gmail.com>
2016-07-06 22:13:20 -0400

    Fix tests

--- test.js
+++ test.js
@@ -2,8 +2,11 @@
 
 var test = require('tape')
 var isDefined = require('./')
+var undefinedValue;
+var definedValue = 'hello';
 
 test(function (t) {
-  t.equal(isDefined('window'), 'typeof window !== "undefined"')
+  t.equal(isDefined(undefinedValue), false)
+  t.equal(isDefined(definedValue), true)
   t.end()
 })
